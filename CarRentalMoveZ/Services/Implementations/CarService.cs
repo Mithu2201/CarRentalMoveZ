d01ecf36558af1c30adb406c203dfb82a558ec70
UT010714 <UT010714@UNICOMTIC71>
2025-09-02 11:23:02 +0530

    Cars

---
+++ CarRentalMoveZ/Services/Implementations/CarService.cs
@@ -0,0 +1,49 @@
+﻿using CarRentalMoveZ.DTOs;
+using CarRentalMoveZ.Repository.Interfaces;
+using CarRentalMoveZ.Services.Interfaces;
+using CarRentalMoveZ.ViewModels;
+
+namespace CarRentalMoveZ.Services.Implementations
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public void AddCar(CarViewModel carViewModel)
+        {
+            var car = CarMapper.MapToEntity(carViewModel);
+            _carRepository.AddCar(car);
+        }
+
+
+        public IEnumerable<CarDTO> GetAll()
+        {
+            var cars = _carRepository.GetAll();
+            return CarMapper.ToDtoList(cars);
+        }
+
+        public CarViewModel GetCarById(int id)
+        {
+            var car = _carRepository.GetCarById(id);
+            return CarMapper.MapToViewModel(car);
+        }
+
+        public void UpdateCar(CarViewModel carViewModel)
+        {
+            var car = CarMapper.MapToEntity(carViewModel);
+            _carRepository.UpdateCar(car);
+        }
+
+        public void DeleteCar(int id)
+        {
+            var car = _carRepository.GetCarById(id);
+            if (car != null)
+            {
+                _carRepository.DeleteCar(car);
+            }
+        }
+    }
+}
---
+++ CarRentalMoveZ/Services/Interfaces/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using CarRentalMoveZ.DTOs;
+using CarRentalMoveZ.ViewModels;
+
+namespace CarRentalMoveZ.Services.Interfaces
+{
+    public interface ICarService
+    {
+        void AddCar(CarViewModel car);
+
+        IEnumerable<CarDTO> GetAll();
+
+        void UpdateCar(CarViewModel car);
+
+        CarViewModel GetCarById(int id);
+
+        void DeleteCar(int id);
+    }
+}
