9c0d54ec08903b928951f5dc851e1e106193bf29
shesanth2005 <uthayachandran0912@gmail.com>
2025-09-14 18:04:18 +0530

    Dynamic Charts

--- CarRentalMoveZ/DTOs/DashboardDTO.cs
+++ CarRentalMoveZ/DTOs/DashboardDTO.cs
@@ -7,5 +7,13 @@
         public int AvailableCars { get; set; }
         public int TotalCars { get; set; }
         public int TotalCustomers { get; set; }
+
+        // Chart data
+        public List<string> EarningsMonths { get; set; }
+        public List<decimal> EarningsRevenue { get; set; }
+
+        public int BookedCount { get; set; }
+        public int PendingCount { get; set; }
+        public int CancelledCount { get; set; }
     }
 }
--- CarRentalMoveZ/Repository/Interfaces/IDashboardRepository.cs
+++ CarRentalMoveZ/Repository/Interfaces/IDashboardRepository.cs
@@ -7,5 +7,9 @@
         Task<int> GetTotalCarsAsync();
         Task<int> GetAvailableCarsAsync();
         Task<int> GetTotalCustomersAsync();
+
+        Task<List<(string Month, decimal Revenue)>> GetMonthlyRevenueAsync();
+
+        Task<(int Booked, int Pending, int Cancelled)> GetBookingStatusCountsAsync();
     }
 }
